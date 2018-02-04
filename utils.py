82eea44411a7511b9484d6a8707a3ed6969daea5
John Kariungi <john.k.gitonga@gmail.com>
2018-02-04 11:21:50 -0800

    Added the Wit.ai API to FB bot app.py POST utils

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from wit import Wit
 access_token = "7VQGABSTV4CNR77DPDLH2FVSIK7ZMTOY"
 
 client = Wit(access_token = access_token)
-message_text = "I like cool places"
+#message_text = "I like cool places"
 
 def wit_response(message_text):
     resp = client.message(message_text)
@@ -16,5 +16,5 @@ def wit_response(message_text):
     except:
         pass
     return (entity, value)
-print(resp)
+#print(resp)
 print(wit_response('I like cool places'))
