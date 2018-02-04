4c2cfff9644559a2724781f5e09c6b2327285737
John Kariungi <john.k.gitonga@gmail.com>
2018-02-04 12:56:40 -0800

    print resp

--- utils.py
+++ utils.py
@@ -9,12 +9,12 @@ def wit_response(message_text):
     resp = client.message(message_text)
     entity = None
     value = None
-
+    print(resp)
     #try:
     entity = list(resp['entities'])[0]
     value = resp['entities']['entity'][0]['value']
     #except Exception as e:
-    #print(e)
+    
     return (entity, value)
 #print(resp)
 print(wit_response('I like cool places'))
